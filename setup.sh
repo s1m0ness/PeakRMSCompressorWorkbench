f5edc51e235008cf0ccf34de3f8cabcdaafa8ddd
s1m0ness <simongal00@gmail.com>
2025-11-30 22:43:15 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ DIR="Source/util/include"
 [ ! -d .git ] && echo "Run from repo root." && exit 1
 
 for f in Config Presets; do
-    if [ ! -f "$DIR/$f.h" ]; then
-        cp "$DIR/$f.user.h" "$DIR/$f.h"
-        echo "Created $f.h"
+    if [ ! -f "$DIR/$f.user.h" ]; then
+        cp "$DIR/$f.h" "$DIR/$f.user.h"
+        echo "Created $f.user.h"
     else
-        echo "$f.h exists"
+        echo "$f.user.h exists"
     fi
 done
 
